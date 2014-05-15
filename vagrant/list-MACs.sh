6832629877f3c77bcead3638c631fa58fa41c3ff
Adam Spiers <aspiers@suse.com>
2014-05-14 23:18:37 -0400

    don't include admin node in vagrant up by default
    For the workshop, we are importing an .ova of the admin node
    in order to save time.
    
    Closes #21.

--- vagrant/list-MACs.sh
+++ vagrant/list-MACs.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for node in admin controller1 controller2 compute1; do
+if [ -z "$VAGRANT_ADMIN_NODE" ]; then
+    nodes=( )
+else
+    nodes=( admin )
+fi
+
+nodes+=( controller1 controller2 compute1 )
+
+for node in "${nodes[@]}"; do
     echo -n "$node: "
     vagrant ssh $node -c 'ip link show eth1 | awk "/ether/ {print \$2}"' -- -q
 done
