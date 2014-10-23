5043bb48a601ad4681d87315c303d41079abd608
Adam Spiers <aspiers@suse.com>
2014-10-23 01:46:08 +0100

    setup-node-aliases.sh: exit with error count

--- vagrant/provisioning/setup-node-aliases.sh
+++ vagrant/provisioning/setup-node-aliases.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 nodes=( $( knife node list | grep '^ *d' ) )
+
+error_count=0
 for node in "${nodes[@]}"; do
     alias=$( ssh -n $node cat .vagrant-guest-name 2>/dev/null )
     if [ -n "$alias" ]; then
@@ -8,5 +10,8 @@ for node in "${nodes[@]}"; do
         crowbar machines rename $node $alias
     else
         echo "WARNING: couldn't retrieve /root/.vagrant-guest-name from $node" >&2
+        error_count=$(( error_count + 1 ))
     fi
 done
+
+exit $error_count
