064d47c839f2821cf7bff416de24698a52a6780d
Adam Spiers <aspiers@suse.com>
2014-10-26 01:44:35 +0100

    make HA disk setup fine-grained and more controllable

--- vagrant/provisioning/controller/setup-glance-NFS.sh
+++ vagrant/provisioning/controller/setup-glance-NFS.sh
@@ -3,11 +3,6 @@
 set -e
 
 admin_ip="$1"
-sbd_device="$2"
-
-# Set up SBD disk
-zypper -n in sbd
-/usr/sbin/sbd -d $sbd_device create
 
 # Mount NFS export for glance
 echo "${admin_ip}:/var/lib/glance /var/lib/glance nfs defaults 0 2" >> /etc/fstab
