f19b4a06617d3cee1ff3a27c2873a097e1c54b15
Adam Spiers <aspiers@suse.com>
2014-10-29 11:45:40 +0000

    fix vagrant provisioner ssh hanging

--- kiwi/cloud-admin/source/root/etc/profile.d/EULA.sh
+++ kiwi/cloud-admin/source/root/etc/profile.d/EULA.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-if [ ! -e $HOME/.eula-accepted ]; then
-  LICENSE_FILE=/etc/YaST2/licenses/SUSE-Beta-EULA-020314.txt
+check_beta_eula () {
+    if [ ! -e $HOME/.eula-accepted ]; then
+        LICENSE_FILE=/etc/YaST2/licenses/SUSE-Beta-EULA-020314.txt
 
-  # Code stolen from
-  # https://github.com/SUSE/studio/blob/master/kiwi-job/templates/SLES11_SP3/root/etc/init.d/suse_studio_firstboot.in
-  stty -nl ixon ignbrk -brkint
+        # Code stolen from
+        # https://github.com/SUSE/studio/blob/master/kiwi-job/templates/SLES11_SP3/root/etc/init.d/suse_studio_firstboot.in
+        stty -nl ixon ignbrk -brkint
 
-  if [ `uname -m` == "s390x" ]; then
-    cat $LICENSE_FILE
-  else
-    less $LICENSE_FILE 2>/dev/null || more $LICENSE_FILE 2>/dev/null || cat $LICENSE_FILE
-  fi
+        if [ `uname -m` == "s390x" ]; then
+            cat $LICENSE_FILE
+        else
+            less $LICENSE_FILE 2>/dev/null || more $LICENSE_FILE 2>/dev/null || cat $LICENSE_FILE
+        fi
 
-  until [ "$answer" == "y" ] || [ "$answer" == "Y" ];
-  do
-    echo -n "Do you accept the EULA? [y/N] "
-    read -e answer
-    if [ "$answer" == "n" ] || [ "$answer" == "N" ]; then
-      sudo halt
+        until [ "$answer" == "y" ] || [ "$answer" == "Y" ];
+        do
+            echo -n "Do you accept the EULA? [y/N] "
+            read -e answer
+            if [ "$answer" == "n" ] || [ "$answer" == "N" ]; then
+                sudo halt
+            fi
+        done
+
+        touch $HOME/.eula-accepted
     fi
-  done
+}
 
-  touch $HOME/.eula-accepted
-fi 
+case "$-" in
+    *i*)
+        check_beta_eula
+        ;;
+esac
