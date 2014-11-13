SUSE-Cloud/suse-cloud-appliances

commit b1c6c1c00ef4cc4286e01ca76dc9a0d22d0f5f75

extract batch_build_proposals()

demos/heat-wordpress/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+: ${PROPOSALS_YAML:=/root/HA-cloud.yaml}
+
 here=$(cd `dirname $0` && pwd)
 source $here/../lib/common.sh
 vagrant_dir=$(cd $here/../../vagrant && pwd)
@@ -49,17 +51,10 @@ main () {
     fi
 
     vagrant_ssh_config
-
     setup_node_aliases
     setup_node_sh_vars
-
     switch_to_qemu_if_required
-
-    if ! vssh admin sudo stdbuf -oL \
-        crowbar batch --timeout 900 build /root/simple-cloud.yaml
-    then
-        die "Failed to set up proposals; aborting"
-    fi
+    batch_build_proposals "$PROPOSALS_YAML"
 
     cd $here
     echo "Downloading appliances to $here ..."

demos/HA/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+: ${PROPOSALS_YAML:=/root/HA-cloud.yaml}
+
 here=$(cd `dirname $0` && pwd)
 source $here/../lib/common.sh
 vagrant_dir=$(cd $here/../../vagrant && pwd)
@@ -53,17 +55,10 @@ main () {
     fi
 
     vagrant_ssh_config
-
     setup_node_aliases
     setup_node_sh_vars
-
     switch_to_qemu_if_required
-
-    if ! vssh admin sudo stdbuf -oL \
-        crowbar batch --timeout 900 build /root/HA-cloud.yaml
-    then
-        die "Failed to set up proposals; aborting"
-    fi
+    batch_build_proposals "$PROPOSALS_YAML"
 
     cat <<'EOF'
 

demos/lib/common.sh
@@ -126,6 +126,16 @@ switch_to_qemu_if_required () {
     fi
 }
 
+batch_build_proposals () {
+    yaml="$1"
+
+    if ! vssh admin sudo stdbuf -oL \
+        crowbar batch --timeout 900 build $yaml
+    then
+        die "Failed to set up proposals; aborting"
+    fi
+}
+
 vssh () {
     ssh -F $VAGRANT_SSH_CONFIG "$@"
 }

commit bc91b4bf933916884246d69cab42e7969799899e

fix heat demo
b1c6c1c accidentally changed the heat demo to use HA-cloud.yaml
but there is no controller2.

demos/heat-wordpress/build.sh
@@ -2,7 +2,7 @@
 
 : ${VAGRANT_CONFIG_FILE:=configs/1-controller-1-compute.yaml}
 export VAGRANT_CONFIG_FILE
-: ${PROPOSALS_YAML:=/root/HA-cloud.yaml}
+: ${PROPOSALS_YAML:=/root/simple-cloud.yaml}
 
 here=$(cd `dirname $0` && pwd)
 source $here/../lib/common.sh