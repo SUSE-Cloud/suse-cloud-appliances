c1225ee593722821b58797ff03071c7f9052a9bb
Adam Spiers <aspiers@suse.com>
2015-05-13 01:34:49 +0100

    add comments explaining REPOS_SKIP_CHECKS

--- vagrant/provisioning/admin/install-suse-cloud.sh
+++ vagrant/provisioning/admin/install-suse-cloud.sh
@@ -3,6 +3,12 @@
 set -e
 
 export PATH="$PATH:/sbin:/usr/sbin/"
+
+# SLES11_SP3 should not be checked since we are using the -DEPS media in lieu
+# SLES11-SP3-Pool is not required due to presence of the -DEPS media (and
+#                 normally isn't due to presence of SP3 media)
+# SUSE-Cloud-5-Pool is not required since it simply duplicates the Cloud media
+# SUSE-Cloud-5-Updates is not required since updates are included in this Cloud media
 export REPOS_SKIP_CHECKS="SLES11_SP3 SLES11-SP3-Pool SUSE-Cloud-5-Pool SUSE-Cloud-5-Updates"
 
 # To trick install-suse-clouds check for "screen". It should be save
