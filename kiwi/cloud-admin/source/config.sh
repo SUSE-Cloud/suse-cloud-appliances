3f2a3469f3ffa7043093d4ba9f87b409edc40124
Vincent Untz <vuntz@suse.com>
2016-04-01 15:56:48 -0400

    - Update everything for SLE 12 SP1 and Cloud 6. - This is more or less a re-creation of the appliance done in   Studio and then re-exported to kiwi.
    Automatically imported from IBS:
    Devel:Cloud:6:Appliances suse-openstack-cloud-admin-appliance
    r3 0.0.1 92b7f18c5f424b73116133e81bf73e83

--- kiwi/cloud-admin/source/config.sh
+++ kiwi/cloud-admin/source/config.sh
@@ -52,16 +52,55 @@ baseSetRunlevel 3
 suseImportBuildKey
 
 
+# Enable sshd
+chkconfig sshd on
+
 #======================================
 # Sysconfig Update
 #--------------------------------------
 echo '** Update sysconfig entries...'
 baseUpdateSysConfig /etc/sysconfig/keyboard KEYTABLE us.map.gz
 baseUpdateSysConfig /etc/sysconfig/network/config FIREWALL no
-baseUpdateSysConfig /etc/sysconfig/network/config NETWORKMANAGER no
+baseUpdateSysConfig /etc/init.d/suse_studio_firstboot NETWORKMANAGER no
 baseUpdateSysConfig /etc/sysconfig/SuSEfirewall2 FW_SERVICES_EXT_TCP 22\ 80\ 443
 baseUpdateSysConfig /etc/sysconfig/console CONSOLE_FONT lat9w-16.psfu
 
+
+#======================================
+# Setting up overlay files 
+#--------------------------------------
+echo '** Setting up overlay files...'
+mkdir -p /root/
+mv /studio/overlay-tmp/files//root//DRBD.yaml /root//DRBD.yaml
+chown root:root /root//DRBD.yaml
+chmod 644 /root//DRBD.yaml
+mkdir -p /etc/YaST2/
+mv /studio/overlay-tmp/files//etc/YaST2//firstboot-suse-openstack-cloud.xml /etc/YaST2//firstboot-suse-openstack-cloud.xml
+chown root:root /etc/YaST2//firstboot-suse-openstack-cloud.xml
+chmod 644 /etc/YaST2//firstboot-suse-openstack-cloud.xml
+mkdir -p /etc/
+mv /studio/overlay-tmp/files//etc//motd /etc//motd
+chown root:root /etc//motd
+chmod 644 /etc//motd
+mkdir -p /root/
+mv /studio/overlay-tmp/files//root//NFS.yaml /root//NFS.yaml
+chown root:root /root//NFS.yaml
+chmod 644 /root//NFS.yaml
+mkdir -p /root/bin/
+mv /studio/overlay-tmp/files//root/bin//node-sh-vars /root/bin//node-sh-vars
+chown root:root /root/bin//node-sh-vars
+chmod 755 /root/bin//node-sh-vars
+mkdir -p /root/bin/
+mv /studio/overlay-tmp/files//root/bin//setup-node-aliases.sh /root/bin//setup-node-aliases.sh
+chown root:root /root/bin//setup-node-aliases.sh
+chmod 755 /root/bin//setup-node-aliases.sh
+mkdir -p /root/
+mv /studio/overlay-tmp/files//root//simple-cloud.yaml /root//simple-cloud.yaml
+chown root:root /root//simple-cloud.yaml
+chmod 644 /root//simple-cloud.yaml
+test -d /studio || mkdir /studio
+cp /image/.profile /studio/profile
+cp /image/config.xml /studio/config.xml
 chown root:root /studio/build-custom
 chmod 755 /studio/build-custom
 # run custom build_script after build
@@ -73,15 +112,9 @@ if ! /studio/build-custom; then
 *********************************
 
 EOF
+
     exit 1
 fi
-
-chown root:root /studio/suse-studio-custom
-chmod 755 /studio/suse-studio-custom
-test -d /studio || mkdir /studio
-
-cp /image/.profile /studio/profile
-cp /image/config.xml /studio/config.xml
 rm -rf /studio/overlay-tmp
 true
 
--- kiwi/cloud-admin/source/root/var/lib/firstboot/setup-node-sh-vars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mv /var/lib/firstboot/node-sh-vars /root/bin
-
-cat <<EOF >>/root/.bash_profile
-if [ -e /tmp/.crowbar-nodes-roles.cache ]; then
-  source /tmp/.crowbar-nodes-roles.cache
-fi
-EOF
