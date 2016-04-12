42aef55305adf1b82f85575ab83612e84aa045db
Vincent Untz <vuntz@suse.com>
2016-04-12 15:48:00 -0400

    Do not install packages on first boot if using live cd
    Automatically imported from IBS:
    Devel:Cloud:6:Appliances suse-openstack-cloud-admin-appliance
    r42 0.0.1 98b8b64a36c0ee03d95c36666e05d616

--- kiwi/cloud-admin/source/root/usr/share/firstboot/scripts/cloud-appliance.sh
+++ kiwi/cloud-admin/source/root/usr/share/firstboot/scripts/cloud-appliance.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # install man, as it's convenient -- not done when building the appliance to
-# keep it smaller
-zypper --non-interactive install man
+# keep it smaller; but skipping if we're on livecd to not abuse disk space
+if [ ! -d /livecd ]; then
+    zypper --non-interactive install man
+fi
 
 # use UTC as timezone, working around broken timezone support for SLE 12 in
 # kiwi...
