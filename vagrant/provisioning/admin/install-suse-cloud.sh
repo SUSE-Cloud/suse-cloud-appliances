b45d0ffc630015a8c433525581ed00a314cfe6ff
Vincent Untz <vuntz@suse.com>
2016-10-21 18:26:28 +0200

    Initial update for Cloud 6

--- vagrant/provisioning/admin/install-suse-cloud.sh
+++ vagrant/provisioning/admin/install-suse-cloud.sh
@@ -4,17 +4,6 @@ set -e
 
 export PATH="$PATH:/sbin:/usr/sbin/"
 
-# SLES11_SP3 should not be checked since we are using the -DEPS media in lieu
-# SLES11-SP3-Pool is not required due to presence of the -DEPS media (and
-#                 normally isn't due to presence of SP3 media)
-# SUSE-Cloud-5-Pool is not required since it simply duplicates the Cloud media
-# SUSE-Cloud-5-Updates is not required since updates are included in this Cloud media
-# SLE11-HAE-SP3-Pool is not required since HAE is included in the -DEPS media
-# SLE11-HAE-SP3-Updates is not required since HAE is included in the -DEPS media
-export REPOS_SKIP_CHECKS="SLES11_SP3 SLES11-SP3-Pool
-                          SUSE-Cloud-5-Pool SUSE-Cloud-5-Updates
-                          SLE11-HAE-SP3-Pool SLE11-HAE-SP3-Updates"
-
 # To trick install-suse-clouds check for "screen". It should be safe
 # to run without screen here, as install-suse-cloud won't pull the network
 # from eth0 because we patched the network cookbook accordingly.
@@ -30,5 +19,5 @@ install -o chef -g chef -m 750 -d /var/run/chef
 install-suse-cloud -v
 
 . /etc/profile.d/crowbar.sh
-crowbar network allocate_ip default cloud5-admin.openstack.site public host
+crowbar network allocate_ip default cloud6-admin.openstack.site public host
 chef-client
--- vagrant/provisioning/admin/prep-admin.sh
+++ vagrant/provisioning/admin/prep-admin.sh
@@ -5,20 +5,4 @@ set -e
 cd /tmp
 
 patch -d /opt/dell -p1 < barclamp-network-ignore-eth0.patch
-patch -d /opt/dell -p1 < barclamp-provisioner-nfs-export.patch
-patch -d /opt/dell -p1 < barclamp-pacemaker-ignore-target-role-changes.patch
 patch -d /opt/dell -p1 < increase-SBD-timeout-30s.patch
-patch -d /opt/dell -p1 < barclamp-provisioner-hae-check.patch
-
-# Scrap pointless 45 second tcpdump per interface
-sed -i 's/45/1/' /opt/dell/chef/cookbooks/ohai/files/default/plugins/crowbar.rb
-
-# Create the directory for shared glance storage
-mkdir -p /var/lib/glance
-
-# Bypass the provisioner's check for HAE repos, since we're already
-# providing these via the special SUSE-CLOUD-SLE11-SP3-DEPS installation media
-# in /srv/tftpboot/suse-11.3/install
-for repo in SLE11-HAE-SP3-{Pool,Updates}; do
-    ln -s ../install /srv/tftpboot/suse-11.3/repos/$repo
-done
