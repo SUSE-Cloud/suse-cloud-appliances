0d5c250dbc6ded8afc240d8fc84d583f9064726b
Vincent Untz <vuntz@suse.com>
2016-11-03 13:58:18 +0100

    Do not leave patches around in /tmp

--- vagrant/provisioning/admin/prep-admin.sh
+++ vagrant/provisioning/admin/prep-admin.sh
@@ -5,4 +5,6 @@ set -e
 cd /tmp
 
 patch -d /opt/dell -p1 < barclamp-network-ignore-eth0.patch
+rm -f barclamp-network-ignore-eth0.patch
 patch -d /opt/dell -p1 < increase-SBD-timeout-30s.patch
+rm -f increase-SBD-timeout-30s.patch
