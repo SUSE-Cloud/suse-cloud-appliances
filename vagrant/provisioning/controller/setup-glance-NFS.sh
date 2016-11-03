54a56957af3c500470733710bdd4c5a25e0924d8
Vincent Untz <vuntz@suse.com>
2016-11-03 15:25:33 +0100

    Install nfs-client before trying to mount NFS shares

--- vagrant/provisioning/controller/setup-glance-NFS.sh
+++ vagrant/provisioning/controller/setup-glance-NFS.sh
@@ -5,6 +5,7 @@ set -e
 admin_ip="$1"
 
 # Mount NFS export for glance
+zypper --non-interactive install nfs-client
 echo "${admin_ip}:/var/lib/glance /var/lib/glance nfs defaults 0 2" >> /etc/fstab
 mkdir -p /var/lib/glance
 mount /var/lib/glance
